fea6d4c8834de091ebc49e9cd02d38049f03c0be
Oleksandr Nikolenko <126420503+NikolenkoOleksandr@users.noreply.github.com>
2023-09-22 14:47:12 +0300

    Add files via upload

---
+++ Lab2/package/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"KI305.Nikolenko.Lab2","l":"Repair"},{"p":"KI305.Nikolenko.Lab2","l":"Shoes"},{"p":"KI305.Nikolenko.Lab2","l":"ShoesApp"},{"p":"KI305.Nikolenko.Lab2","l":"Wash"}];updateSearchResults();
