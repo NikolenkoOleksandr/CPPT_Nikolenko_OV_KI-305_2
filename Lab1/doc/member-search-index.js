9fe1f2b88814cd70019c37af3885c8e7360a2135
Oleksandr Nikolenko <126420503+NikolenkoOleksandr@users.noreply.github.com>
2023-09-24 20:14:34 +0300

    Delete Lab1 directory

--- Lab1/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"Lab1","l":"Lab1()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Lab1","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
